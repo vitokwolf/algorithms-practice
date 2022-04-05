088a0838e3d913c22ddd45396a4ce4a4470ea1a0
Victor Lupascu <sinajeen@gmail.com>
2022-04-05 18:30:49 -0500

    solved is-palindrome

--- 04-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
+++ 04-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,13 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  let reverseStr = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i]
+  }
+  if (str === reverseStr) {
+    return true
+  } else {
+    return false
+  }
+}
