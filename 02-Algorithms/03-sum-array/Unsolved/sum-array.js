d6d07e631b477eeb9ff9cacc4e98a9c675eb1629
Victor Lupascu <sinajeen@gmail.com>
2022-04-05 13:49:21 -0500

    solved sum-array

--- 02-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ 02-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,8 @@
 // Write code to add all the numbers in `arr` and return the total
-
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  let result = 0
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i]
+  }
+  return result
+}
