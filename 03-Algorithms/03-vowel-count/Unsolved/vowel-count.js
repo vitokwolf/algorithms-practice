91dd300ead1cfc37ffe2f01dbf3aebd7ed0f2765
Victor Lupascu <sinajeen@gmail.com>
2022-04-05 14:24:16 -0500

    solve vowel-count

--- 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 03-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,19 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  let counter = 0
+  str = str.toLowerCase()
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === 'a' ||
+      str[i] === 'e' ||
+      str[i] === 'i' ||
+      str[i] === 'o' ||
+      str[i] === 'u' ||
+      str[i] === 'y'
+    ) {
+      counter++
+    }
+  }
+  return counter
+}
