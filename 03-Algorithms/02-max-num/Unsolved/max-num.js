dc234f9540ab5cedcb10ee74f3fa0178de44aa70
Victor Lupascu <sinajeen@gmail.com>
2022-04-05 14:15:46 -0500

    solve max-num

--- 03-Algorithms/02-max-num/Unsolved/max-num.js
+++ 03-Algorithms/02-max-num/Unsolved/max-num.js
@@ -1,3 +1,6 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function (arr) {
+  arr.sort((a, b) => a - b)
+  return arr[arr.length - 1]
+}
