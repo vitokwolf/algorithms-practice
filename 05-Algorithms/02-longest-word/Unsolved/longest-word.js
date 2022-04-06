8016310894cbb6809bb9e8014190fe62d16c3e46
Victor Lupascu <sinajeen@gmail.com>
2022-04-06 15:05:39 -0500

    solve longest-word

--- 05-Algorithms/02-longest-word/Unsolved/longest-word.js
+++ 05-Algorithms/02-longest-word/Unsolved/longest-word.js
@@ -1,3 +1,14 @@
 // Write code to create a function that accepts a string (`str`) and returns the longest word in the string
 
-var longestWord = function(str) {};
+var longestWord = function (str) {
+  let result = ''
+  let arr = str.split(' ')
+  //   console.log(str)
+  //   console.log(arr)
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > result.length) {
+      result = arr[i]
+    }
+  }
+  return result
+}
