2d56cb18bc6a8b7f03832347387f46bef4103f10
Victor Lupascu <sinajeen@gmail.com>
2022-04-06 16:44:06 -0500

    solve came-case

--- 06-Algorithms/03-camel-case/Unsolved/camel-case.js
+++ 06-Algorithms/03-camel-case/Unsolved/camel-case.js
@@ -1,3 +1,24 @@
 // Write code to create a function that accepts a string and returns the string in camelCase
 
-var camelCase = function(str) {};
+var camelCase = function (str) {
+  let result = ''
+  let arr = str.split(' ')
+
+  for (let i = 0; i < arr.length; i++) {
+    let word = arr[i].toLowerCase()
+
+    if (i === 0) {
+      result += word
+    } else {
+      for (let x = 0; x < word.length; x++) {
+        if (x === 0) {
+          result += word[0].toUpperCase()
+        } else {
+          result += word[x]
+        }
+      }
+    }
+  }
+
+  return result
+}
