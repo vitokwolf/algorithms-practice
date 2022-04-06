58f52a460ff3d04d82f6eacff13a5076694c8e99
Victor Lupascu <sinajeen@gmail.com>
2022-04-06 15:21:54 -0500

    solve reverse-words

--- 05-Algorithms/04-reverse-words/Unsolved/reverse-words.js
+++ 05-Algorithms/04-reverse-words/Unsolved/reverse-words.js
@@ -1,3 +1,16 @@
 // Write code to create a function that accepts a string (`str`) and reverses the order of the words in the string
 
-var reverseWords = function(str) {};
+var reverseWords = function (str) {
+  let reverseStr = ''
+  let words = str.split(' ')
+  for (let i = words.length - 1; i >= 0; i--) {
+    let word = words[i]
+
+    if (i === 0) {
+      reverseStr += word
+    } else {
+      reverseStr += word + ' '
+    }
+  }
+  return reverseStr
+}
