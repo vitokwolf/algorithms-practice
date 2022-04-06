df706d242b786d26c805d4e0dd415066993e9227
Victor Lupascu <sinajeen@gmail.com>
2022-04-06 14:59:05 -0500

    solve swap-case

--- 05-Algorithms/01-swap-case/Unsolved/swap-case.js
+++ 05-Algorithms/01-swap-case/Unsolved/swap-case.js
@@ -1,3 +1,15 @@
 // Write code to create a function takes a string and returns the string with all of the letter cases swapped
 
-var swapCase = function(str) {};
+var swapCase = function (str) {
+  let resultStr = ''
+  for (let i = 0; i < str.length; i++) {
+    let capitalized = str[i].toUpperCase()
+
+    if (str[i] === capitalized) {
+      resultStr += str[i].toLowerCase()
+    } else {
+      resultStr += str[i].toUpperCase()
+    }
+  }
+  return resultStr
+}
