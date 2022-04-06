f63c7acba587e2bca05d82cfbd5eb9196ec218dd
Victor Lupascu <sinajeen@gmail.com>
2022-04-06 15:11:56 -0500

    solve acronym-builder

--- 05-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
+++ 05-Algorithms/03-acronym-builder/Unsolved/acronym-builder.js
@@ -1,3 +1,11 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-var acronymBuilder = function(str) {};
+var acronymBuilder = function (str) {
+  let acronym = ''
+  let words = str.split(' ')
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i]
+    acronym += word[0].toUpperCase()
+  }
+  return acronym
+}
