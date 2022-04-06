ad0615488d1e7e243fa182e1823d09e69ba1cd1e
Victor Lupascu <sinajeen@gmail.com>
2022-04-05 19:39:25 -0500

    solve factorial

--- 04-Algorithms/02-factorial/Unsolved/factorial.js
+++ 04-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+  let factorial = 1
+  if (num === 0) {
+    return factorial
+  } else {
+    for (let i = 1; i <= num; i++) {
+      factorial = factorial * i
+    }
+  }
+  return factorial
+}
