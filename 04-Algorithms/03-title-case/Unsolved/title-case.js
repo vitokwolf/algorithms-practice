4a646c6d67d0c20098dac7682803e6507b09ebf5
Victor Lupascu <sinajeen@gmail.com>
2022-04-05 19:52:56 -0500

    solve title-case

--- 04-Algorithms/03-title-case/Unsolved/title-case.js
+++ 04-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,13 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function (str) {
+  let result = ''
+  for (let i = 0; i < str.length; i++) {
+    if (i === 0 || str[i - 1] === ' ') {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
